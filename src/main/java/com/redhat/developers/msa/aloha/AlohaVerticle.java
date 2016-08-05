5b28b12988900d4ffd99f55ab0e363e230d3c049
adrianbele <adrianbele@users.noreply.github.com>
2016-08-05 14:47:47 +0200

    Update AlohaVerticle.java

--- src/main/java/com/redhat/developers/msa/aloha/AlohaVerticle.java
+++ src/main/java/com/redhat/developers/msa/aloha/AlohaVerticle.java
@@ -71,7 +71,7 @@ public class AlohaVerticle extends AbstractVerticle {
 
         // Health Check
         router.get("/api/health").handler(ctx -> {
-            ctx.response().end("I'm ok now 1000000000");
+            ctx.response().end("I'm ok now 2000000000");
         });
 
         // Hysrix Stream Endpoint
